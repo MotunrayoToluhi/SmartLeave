929d246d4fd6e05ad16dd65c27b1dbb16d358f7c
Katevb2 <katevb2@gmail.com>
2023-11-29 16:07:00 +1100

    Creating files and pushing initial code to classes_file.py, database.sql and Db_utils.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+# Greet user
+name = input("What's your name? ")
+greeting_framework = "Welcome to SmartLeave, "
+greeting_full = greeting_framework + name
+print(greeting_full)
+
+# Either access database using user's name or create database for them...
+
