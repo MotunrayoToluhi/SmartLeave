e248f4a9276874da0750662c056b7c5e11fb4b92
MotunrayoToluhi <147548250+MotunrayoToluhi@users.noreply.github.com>
2023-12-04 12:05:15 -0800

    finished function for sorting dates from api by season

--- LeavePlannerLogic/API_Logic/Holiday_api.py
+++ LeavePlannerLogic/API_Logic/Holiday_api.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from Static_data.DateCategories import seasons_data
 
 
+# app.route()
 def get_API_holidays():
     url = 'https://www.gov.uk/bank-holidays.json'
     response = requests.get(url)
@@ -12,10 +13,12 @@ def get_API_holidays():
 
 holidays = get_API_holidays()
 
-winter_holidays = {}
-spring_holidays = {}
-summer_holidays = {}
-autumn_holidays = {}
+holidays_by_season = {
+    "winter_holidays": [],
+    "spring_holidays": [],
+    "summer_holidays": [],
+    "autumn_holidays": []
+}
 
 
 def categorize_by_season():
@@ -28,44 +31,14 @@ def categorize_by_season():
         for season, months in seasons_data.items():
             for month, _ in months:
                 if event_date.month == month:
-                    seasons_data[season].append({
+                    holidays_by_season[season + "_holidays"].append({
                         "title": event_title,
                         "date": event_date_str
                     })
                     break
 
+
 categorize_by_season()
 
-print()
-
-# for season_name, events in seasons_data.items():
-#     seasons_data[season_name] = sorted(events, key=lambda x: x["date"])
-
-
-# def categorize_by_season(event_title, event_date, seasons_data):
-# def categorize_by_season():
-#     for event in holidays:
-#         event_date = event["date"]
-#         event_title = event["title"]
-#     date_format = datetime.strptime(event_date, "%Y-%M-%D")
-#     for season, months in seasons_data.items()
-#         for month, i in months:
-#             if date_format == month:
-#                 season_data[season]
-
-
-# Test to see if righ info is given
-# print(event_title)
-# print(event_date)
-# print("---------")
-
-#     event_date = datetime.strptime(event_date, "%Y-%m-%d")
-#     for season, months in seasons_data.items():
-#         for month, i in months:
-#             if event_date.month == month:
-#                 seasons_data[season].append(event_date.strftime("%Y-%m-%d"))
-
-
-# print("Title:", event["title"])
-# print("Date:", event["date"])
-
+for key, holidays_list in holidays_by_season.items():
+    print(f"{key}: {holidays_list}")
