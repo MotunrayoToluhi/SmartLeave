664ea061a136fda4fa3d87b00c5478d0eeff2558
tenibalogun <tenibalogun1@gmail.com>
2023-12-13 19:14:17 +0000

    adding and checking of users to db class

--- Database/Db_utils.py
+++ Database/Db_utils.py
@@ -17,33 +17,33 @@ def _connect_to_db(db_name):
     return cnx
 
 
-# Try out helper option
-def get_user_data_by_name(current_name):
-    try:
-        db_name = "TestData"
-        db_connection = _connect_to_db(db_name)
-        cur = db_connection.cursor()
-        # print("Connected to DB")
-
-        query = "SELECT * from users"
-
-        cur.execute(query)
-
-        result = cur.fetchall()
-
-        cur.close()
-
-    except Exception:
-        raise DbConnectionError("Failed to read data")
-
-    finally:
-        if db_connection:
-            db_connection.close()
-            # print("DB connection is closed")
-
-    return result
-
-
-current_name = 'Ayo'
-get_user_data_by_name(current_name)
-
+# # Try out helper option
+# def get_user_data_by_name(current_name):
+#     try:
+#         db_name = "TestData"
+#         db_connection = _connect_to_db(db_name)
+#         cur = db_connection.cursor()
+#         # print("Connected to DB")
+#
+#         query = "SELECT * from users"
+#
+#         cur.execute(query)
+#
+#         result = cur.fetchall()
+#
+#         cur.close()
+#
+#     except Exception:
+#         raise DbConnectionError("Failed to read data")
+#
+#     finally:
+#         if db_connection:
+#             db_connection.close()
+#             # print("DB connection is closed")
+#
+#     return result
+#
+#
+# current_name = 'Ayo'
+# get_user_data_by_name(current_name)
+#
