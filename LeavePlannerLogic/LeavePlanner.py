d083b746bb409d9d26127ea76305201fee802410
MotunrayoToluhi <147548250+MotunrayoToluhi@users.noreply.github.com>
2023-12-17 09:36:03 -0800

    deleting unused code

--- LeavePlannerLogic/LeavePlanner.py
+++ LeavePlannerLogic/LeavePlanner.py
@@ -13,8 +13,6 @@ from LeavePlannerLogic.Holiday_api import categorized_holiday
 class LeavePlannerFunc:
 
     def __init__(self):
-        # self.next_bank = self.next_bank_holiday()
-        # self.birthday = self.birthday_off()
         self.max_leave = self.leave_entitlement()
         self.days_of_holiday = self.holiday_length()
         self.holiday_year = self.user_chosen_year()
@@ -26,52 +24,8 @@ class LeavePlannerFunc:
         self.date_list = [holiday["date"] for holiday in self.filtered_year_season[self.holiday_season]]
 
         self.date_obj_list = [datetime.strptime(date_str, "%Y-%m-%d") for date_str in self.date_list]
-        # self.calc_holiday_date = self.calc_holiday_date()
         self.run_leave_planner = self.run_leave_planner
 
-    # function to get next bank holiday
-    # def next_bank_holiday(bank_hol):
-    #     today = datetime.datetime.now().date()
-    #     next_one = min(bank_holidays, key=lambda x: abs(x - today))
-    #     return next_one
-    #
-    # # function check if birthday lands on weekend or AL or neither
-    # def birthday_off(birthday):
-    #     date_birthday = datetime.datetime.strptime(birthday, '%Y-%m-%d').date()
-    #     print(date_birthday)
-    #     bday_hol = any(date_birthday == num for num in bank_holidays)
-    #     print(bday_hol)
-    #     if bday_hol == False:
-    #         if date_birthday.weekday() >= 5:
-    #             return True
-    #         elif date_birthday.weekday() < 5:
-    #             return False
-    #     else:
-    #         return True
-    #
-    # # need to re-write this in to __init__
-    # def maximise(listofbankhols, year):
-    #     # getting just that years holidays plus new years day
-    #     next_year = int(year) + 1
-    #     the_year = datetime.datetime.strptime(year, '%Y').date()
-    #     next_year = datetime.datetime.strptime(str(next_year), '%Y').date()
-    #     this_years_bank_hols = [date for date in listofbankhols if the_year <= date <= next_year]
-    #     time_differences = []
-    #     # getting the lenght of time between bank holidays
-    #     l = len(this_years_bank_hols)
-    #     seven_days = datetime.timedelta(days=7)
-    #     for i in range(l):
-    #         for x in range(i + 1, l):
-    #             days = this_years_bank_hols[x] - this_years_bank_hols[i]
-    #             # get all bank holidays that have less than a week between them
-    #             if days <= seven_days:
-    #                 return (days, this_years_bank_hols[x], this_years_bank_hols[i])
-                # print(this_years_bank_hols[x], this_years_bank_hols[i], days)
-
-    # print(maximise(bank_holidays, '2024'))
-
-
-    # asks user max leave entitlement they have
     def leave_entitlement(self):
         while True:
             self.max_leave = input(f"How many leave days are you entitled to per year?: ")
@@ -106,20 +60,6 @@ class LeavePlannerFunc:
             else:
                 print("year must be 2023, 2024 or 2025")
         return preferred_year
-
-    # asks user how many days notice they need to give their employer before booking a holiday
-    #     def days_notice(self):
-    #         while True:
-    #             user_days_notice = input(f"How many days notice do you need to give your employer?: ")
-    #             if user_days_notice.isdigit():
-    #                 user_days_notice = int(user_days_notice)
-    #                 if 0 < user_days_notice < 365:
-    #                     break
-    #                 else:
-    #                     print("Days must be more than zero and less than 365.")
-    #             else:
-    #                 print(f"Please enter a valid positive number.")
-    #         return user_days_notice
 
     # asks user how long they want their holiday to be
     def holiday_length(self):
@@ -195,12 +135,9 @@ class LeavePlannerFunc:
                 leave_days_used = numpy.busday_count(hol_start, hol_end) - 1
                 leave_saved = self.days_of_holiday - leave_days_used
                 print("You have used", leave_days_used, "leave days and have saved", leave_saved, "days")
+    
     def run_leave_planner(self):
-
         self.leave_days_used()
-
-
-
 
 
 
